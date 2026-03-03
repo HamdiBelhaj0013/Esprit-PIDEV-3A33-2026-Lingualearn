455303e80f0843feeb69162e5b826b02602e29c6
HamdiBelhaj0013 <belhajhamdi185@gmail.com>
2026-03-04 00:23:16 +0100

    cleanup: remove temp files and dev notes

--- temp_fix_options.sql
+++
@@ -1,2 +0,0 @@
-UPDATE test_question SET options = '{"A": "Option A", "B": "Option B", "C": "Option C", "D": "Option D"}'::json WHERE (options = '[]' OR options IS NULL OR options = '{}') AND question_type IN ('qcm', 'qcm_single', 'qcm_multiple');
-
